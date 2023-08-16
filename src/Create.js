a358be1776b985fcabe880001627c22b917fe58a
Dhairya Shah <dhairyaj2004@gmail.com>
2023-08-16 11:01:45 +0530

    test

--- src/Create.js
+++ src/Create.js
@@ -5,22 +5,22 @@ const Create = () => {
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("");
   const [topic, setTopic] = useState("");
-  const [isLoading,setisLoading]=useState(false)
-  const history=useHistory()
-  const handlesubmit=(e)=>{
-    e.preventDefault()
-    setisLoading(true)
-    const blog={title,body,author,topic}
-    fetch('http://localhost:8000/blogs',{
-      method: 'POST',//GET method is use to request resources from server and post method is used to add data on end point
-      headers:{'Content-Type':"application/json"},//other types like content type are authentication & authorization,Encoding
-      body:JSON.stringify(blog)
-    }).then(()=>{
-      console.log("New blog added!!!")
-      setisLoading(false)
-      history.push("/")//u can use history.go() method also if u write history.go(-1) then it go back one step if 1 then move to next step
-    })
-  }
+  const [isLoading, setisLoading] = useState(false);
+  const history = useHistory();
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setisLoading(true);
+    const blog = { title, body, author, topic };
+    fetch("http://localhost:8000/blogs", {
+      method: "POST", //GET method is use to request resources from server and post method is used to add data on end point
+      headers: { "Content-Type": "application/json" }, //other types like content type are authentication & authorization,Encoding
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New blog added!!!");
+      setisLoading(false);
+      history.push("/"); //u can use history.go() method also if u write history.go(-1) then it go back one step if 1 then move to next step
+    });
+  };
   return (
     <div className="create">
       <h1>Add New Blog</h1>
@@ -56,12 +56,13 @@ const Create = () => {
           }}
         />
         <label>Blog body</label>
-        <textarea 
-        required 
-        value={body}
+        <textarea
+          required
+          value={body}
           onChange={(e) => {
             setBody(e.target.value);
-          }}></textarea>
+          }}
+        ></textarea>
         {!isLoading && <button>Publish Blog</button>}
         {isLoading && <button disabled>Publishing Blog...</button>}
       </form>
